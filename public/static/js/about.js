58e8e8b159aa394c6556217f153db26bbba617e7
Smite Chow <xiaopengyou@live.com>
2025-03-26 03:58:53 +0000

    improve about page jsonld

--- public/static/js/about.js
+++ public/static/js/about.js
@@ -4,13 +4,33 @@ const config = await response.json()
 utils.renderAvatar(config)
 utils.renderJSONLD({
   "@context": "https://schema.org",
-  "@type": "Person",
-  "name": config.displayName,
-  "jobTitle": "Blogger",
-  "description": config.description,
-  "sameAs": [
-    `${location.origin}/about`,
-    `https://${config.app_host}/profile/${config.handle}`
-  ]
+  "@type": "ProfilePage",
+  "dateCreated": config.createdAt,
+  "dateModified": config.indexedAt,
+  "mainEntity": {
+    "@type": "Person",
+    "name": config.displayName,
+    "identifier": config.did,
+    "interactionStatistic": [{
+      "@type": "InteractionCounter",
+      "interactionType": "https://schema.org/FollowAction",
+      "userInteractionCount": config.followersCount
+    }],
+    "agentInteractionStatistic": [{
+      "@type": "InteractionCounter",
+      "interactionType": "https://schema.org/WriteAction",
+      "userInteractionCount": config.postsCount
+    },{
+      "@type": "InteractionCounter",
+      "interactionType": "https://schema.org/FollowAction",
+      "userInteractionCount": config.followsCount
+    }],
+    "description": config.description,
+    "image": config.avatar,
+    "sameAs": [
+      `${location.origin}/about`,
+      `https://${config.app_host}/profile/${config.handle}`
+    ]
+  }
 })
 await utils.initBackgroundPic()
